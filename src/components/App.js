117435d6560b63a6428664019ebb19d8f5747530
001pride <alxndrthepride@gmail.com>
2025-12-16 01:26:44 +0100

    Initial Commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Button from "./components/Button";
+
+function App() {
+  return (
+    <div style={{ padding: "40px" }}>
+      <Button>Primary Button</Button>
+      <br /><br />
+      <Button variant="secondary">Secondary Button</Button>
+    </div>
+  );
+}
+
+export default App;
